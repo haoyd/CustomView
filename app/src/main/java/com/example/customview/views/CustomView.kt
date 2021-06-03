a8c25f0625713cb79aa68467f978109a3f007f7a
hyd <eric_hyd@foxmail.com>
2021-06-03 18:16:57 +0800

    feat：可拖拽功能

--- app/src/main/java/com/example/customview/views/CustomView.kt
+++ app/src/main/java/com/example/customview/views/CustomView.kt
@@ -4,14 +4,20 @@ import android.content.Context
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 import com.blankj.utilcode.util.ConvertUtils
+import com.blankj.utilcode.util.LogUtils
 
 open class CustomView : View {
 
     private val sideWidth = ConvertUtils.dp2px(2f).toFloat()
 
     protected val mPaint = Paint()
+
+    protected var parentWidth: Int = 0
+    protected var parentHeight: Int = 0
 
     protected var cx: Float = 0f
     protected var cy: Float = 0f
@@ -22,6 +28,8 @@ open class CustomView : View {
     protected var mRight: Float = 0f
     protected var mBottom: Float = 0f
 
+    private var sx = 0
+    private var sy = 0
 
     constructor(context: Context?) : this(context, null)
     constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0)
@@ -35,6 +43,9 @@ open class CustomView : View {
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        parentWidth = (parent as ViewGroup).measuredWidth
+        parentHeight = (parent as ViewGroup).measuredHeight
+
         cx = measuredWidth / 2f
         cy = measuredHeight / 2f
         radius = cx - sideWidth
@@ -44,6 +55,64 @@ open class CustomView : View {
         mRight = measuredWidth - sideWidth
         mBottom = measuredHeight - sideWidth
     }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if (event == null) {
+            return false
+        }
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                sx = event.rawX.toInt()
+                sy = event.rawY.toInt()
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val x = event.rawX
+                val y = event.rawY
+
+                // 手指移动距离
+                val dx = x - sx
+                val dy = y - sy
+
+                var l = left + dx
+                var t = top + dy
+                var r = right + dx
+                var b = bottom + dy
+
+                if (l < 0) {
+                    l = 0f
+                    r = width.toFloat()
+                }
+
+                if (t < 0) {
+                    t = 0f
+                    b = height.toFloat()
+                }
+
+                if (r > parentWidth) {
+                    r = parentWidth.toFloat()
+                    l = parentWidth - width.toFloat()
+                }
+
+                if (b > parentHeight) {
+                    b = parentHeight.toFloat()
+                    t = parentHeight - height.toFloat()
+                }
+
+                layout(l.toInt(), t.toInt(), r.toInt(), b.toInt())
+
+                sx = x.toInt()
+                sy = y.toInt()
+            }
+            MotionEvent.ACTION_UP -> {
+            }
+            else -> {
+            }
+        }
+
+        return true
+    }
+
 
     private fun init() {
         mPaint.isAntiAlias = true
