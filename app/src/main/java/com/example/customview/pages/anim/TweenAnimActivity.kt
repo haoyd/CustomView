1c697141a985e05cede8b820522e671bcf229ead
hyd <eric_hyd@foxmail.com>
2021-06-04 15:23:39 +0800

    feat：补间动画组

--- app/src/main/java/com/example/customview/pages/anim/TweenAnimActivity.kt
+++ app/src/main/java/com/example/customview/pages/anim/TweenAnimActivity.kt
@@ -3,10 +3,7 @@ package com.example.customview.pages.anim
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.animation.AlphaAnimation
-import android.view.animation.AnimationUtils
-import android.view.animation.RotateAnimation
-import android.view.animation.ScaleAnimation
+import android.view.animation.*
 import com.example.customview.BaseActivity
 import com.example.customview.R
 import kotlinx.android.synthetic.main.activity_tween_anim.*
@@ -21,19 +18,37 @@ class TweenAnimActivity : BaseActivity() {
         val anim = AnimationUtils.loadAnimation(this, R.anim.anim_translate)
         mStar.startAnimation(anim)
     }
+
     fun scale(view: View) {
         val anim = ScaleAnimation(0f, 2f, 0f, 2f)
         anim.duration = 3000
         mStar.startAnimation(anim)
     }
+
     fun rotate(view: View) {
         val anim = RotateAnimation(0f, 360f)
         anim.duration = 3000
         mStar.startAnimation(anim)
     }
+
     fun alpha(view: View) {
         val anim = AlphaAnimation(0f, 1f)
         anim.duration = 3000
         mStar.startAnimation(anim)
+    }
+
+    fun group(view: View) {
+        val animSet = AnimationSet(this, null)
+
+        val anim1 = ScaleAnimation(0f, 2f, 0f, 2f)
+        anim1.duration = 3000
+
+        val anim2 = RotateAnimation(0f, 360f)
+        anim2.duration = 3000
+
+        animSet.addAnimation(anim1)
+        animSet.addAnimation(anim2)
+        
+        mStar.startAnimation(animSet)
     }
 }
