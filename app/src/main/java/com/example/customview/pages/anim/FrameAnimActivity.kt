4aa3818b2ab2b65574a5a4a172262ab0a0922765
hyd <eric_hyd@foxmail.com>
2021-06-04 15:41:52 +0800

    feat：帧动画

--- app/src/main/java/com/example/customview/pages/anim/FrameAnimActivity.kt
+++ app/src/main/java/com/example/customview/pages/anim/FrameAnimActivity.kt
@@ -1,13 +1,29 @@
 package com.example.customview.pages.anim
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.customview.BaseActivity
 import com.example.customview.R
+import kotlinx.android.synthetic.main.activity_frame_anim.*
 
 class FrameAnimActivity : BaseActivity() {
+
+    private lateinit var animDrawable: AnimationDrawable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frame_anim)
+
+        animDrawable = frameImg.background as AnimationDrawable
+    }
+
+    fun startAnim(view: View) {
+        animDrawable.start()
+    }
+
+    fun stopAnim(view: View) {
+        animDrawable.stop()
     }
 }
